2dbd93e9b78155f62bfb5a1acc3c49c4bf13f205
Tanushreehurali <tanuhurali@gmail.com>
2026-01-16 09:40:36 +0530

    save

--- factorial.py
+++ factorial.py
@@ -1,14 +1,14 @@
-def factorial():
-    n=int(input("enter a number:"))
+def get_number():
+    return int(input("Enter a number: "))
 
 def display_factorial(n):
-    fact=1
-    for i in range(1,n+1):
-        fact*=i
-    print("factorial:",fact)
+    fact = 1
+    for i in range(1, n+1):
+        fact *= i
+    print(f"\nFactorial of {n} is {fact}")
 
-if __name__ == "__main__" :
-    n = factorial()
+if __name__ == "__main__":
+    n = get_number()
     display_factorial(n)
 
 
