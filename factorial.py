bdaa8cdb9e40a488e67fd6a513af96bd924fe52c
Tanushreehurali <tanuhurali@gmail.com>
2026-01-16 09:26:55 +0530

    save

---
+++ factorial.py
@@ -0,0 +1,21 @@
+def factorial():
+    n=int(input("enter a number:"))
+
+def display_factorial(n):
+    fact=1
+    for i in range(1,n+1):
+        fact*=i
+    print("factorial:",fact)
+
+if __name__ == "__main__" :
+    n = factorial()
+    display_factorial(n)
+
+
+
+
+
+
+
+
+
