3645025ada56768c8aee276f6a4bee462417b7ec
Tanushreehurali <tanuhurali@gmail.com>
2026-01-16 10:39:05 +0530

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -1,20 +1,15 @@
-def get_number():
-    return int(input("Enter a number: "))
+def factorial():
+    n=int(input("Enter a number: "))
 
 def display_factorial(n):
     fact = 1
     for i in range(1, n+1):
         fact *= i
-    print(f"\nFactorial of {n} is {fact}")
+    print("factorial:",fact)
 
 if __name__ == "__main__":
-    n = get_number()
+    n = factorial()
     display_factorial(n)
-
-
-
-
-
 
 
 
